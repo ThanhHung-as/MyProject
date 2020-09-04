f66b3c6632e62ae8cefd8840f0159dc0b9681e9d
ThanhHung-as <70557220+ThanhHung-as@users.noreply.github.com>
2020-09-04 21:21:31 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("My name is Hưng");
 	}
 }
